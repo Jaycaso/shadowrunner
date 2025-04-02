d7cd736261079359703ac1a3828a8f52c7c02759
Jaycaso <justin.louissaint21@gmail.com>
2025-04-01 21:01:04 -0400

    Capsule and Plane added
    Test player and test plane added

---
+++ My project (2)/Assets/Scripts/Playercontroller.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float forwardSpeed = 5f;
+    public float jumpForce = 7f;
+    private Rigidbody rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        // Move forward automatically
+        transform.position += Vector3.forward * forwardSpeed * Time.deltaTime;
+
+        // Jump input
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
+
